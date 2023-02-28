bb3472454e767f96503fc20342989dd8d739a58a
baoat <baoat191201@gmail.com>
2023-02-28 16:43:45 +0700

    Tạo form Login và cấu hình project

---
+++ clinic-frontend/src/page/Login.jsx
@@ -0,0 +1,29 @@
+import './Login.css'
+
+export const Login = () => {
+    return(
+        <div className="App">
+        <img className="logo" alt="Business view - Reports" />
+        <form className="form">
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="nome@gmail.com" />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">PASSWORD</label>
+            <input type="password" name="password" />
+          </div>
+          <button className="primary">ENTRAR</button>
+        </form>
+        <div className='forgot-password' >
+            <div className='forgot-password-handle' >
+                <a className='underline text-blue-700' href='#' >Quên mật khẩu</a>
+            </div>
+            <div className='create-account' >
+                <span>Bạn chưa có tài khoản <a className='underline text-blue-700' href='#' >Tạo ngay</a></span>
+            </div>
+            
+        </div>
+      </div>
+    )
+}
