cb04c8bbc666402a1f40e743beb150e7cd918d11
baoat <baoat191201@gmail.com>
2023-02-28 17:07:42 +0700

    Tạo file Register

--- clinic-frontend/src/App.js
+++ clinic-frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import {Route, Routes, BrowserRouter} from "react-router-dom"
 import { Login } from './page/Login';
+import { Register } from './page/Register';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/signIn' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
       </Routes>
     </BrowserRouter>
     </>
