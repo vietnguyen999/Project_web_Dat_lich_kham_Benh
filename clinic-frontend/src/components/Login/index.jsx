889e1420f8bb05c875ed6d3cfe338911376f6f7c
baoat <baoat191201@gmail.com>
2023-02-28 11:23:39 +0700

    Setup thư viện

---
+++ clinic-frontend/src/components/Login/index.jsx
@@ -0,0 +1,16 @@
+export const Login = () => {
+    <div >
+        <span>Login</span>
+        <div>
+            <label>Username</label>
+            <input type='text' />
+        </div>
+        <div>
+            <label>Password</label>
+            <input type='password' />
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </div>
+}
