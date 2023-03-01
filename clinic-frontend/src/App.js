4fe1f5d2a24cfe8aeffe9fbcd785a5d9b897bf15
vietnguyen99 <nguyenducviet888999@gmail.com>
2023-03-01 10:18:21 +0700

    lay file cua bao

--- clinic-frontend/src/App.js
+++ clinic-frontend/src/App.js
@@ -1,13 +1,29 @@
 
 import React from 'react'
-import Nabar from './Container/Nabar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './containet/Home'
+import About from './containet/About'
+import Blog from './containet/Blog'
+import News from './containet/News';
+import Contact from './containet/Contact';
+import Nabar from './containet/Nabar'
 
 function App() {
   return (
-    <React.Fragment>
-			<Nabar/>
-		</React.Fragment>
-  )
+    <div className="App">
+      <Nabar />
+      <Routes>
+      <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/contact" element={<Contact />} />
+      
+      </Routes>
+      
+      
+    </div>
+  );
 }
-
-export default App
+export default App;
---
+++ clinic-frontend/src/containet/Nabar.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {Navbar,Container,Nav,Form,Button} from 'react-bootstrap';
+import { Link } from "react-router-dom";
+function Nabar() {
+  return (
+    
+    <div>
+    <Navbar bg="dark" variant={"dark"} expand="lg">
+   <Container fluid>
+   <Navbar.Brand href="#">Logo  </Navbar.Brand>
+   <Navbar.Toggle aria-controls="navbarScroll" />
+   <Navbar.Collapse id="navbarScroll">
+     {/* <Nav
+       className=" mx-auto my-2 my-lg-0"
+       style={{ maxHeight: '100px' }}
+       navbarScroll > */}
+        <Nav className="justify-content-end flex-grow-1 pe-3 navx">
+        <Nav.Link className='navbare' as={Link} to={"/"}>Trang Chủ</Nav.Link>
+       <Nav.Link  className='navbare' as={Link} to={"/about"}>Giới Thiệu</Nav.Link>
+       <Nav.Link  className='navbare' as={Link} to={"/blog"}>Dịch Vụ</Nav.Link>
+       <Nav.Link  className='navbare' as={Link} to={"/news"}>Tin Tức</Nav.Link>
+       <Nav.Link  className='navbare' as={Link} to={"/blog"}>Liên Hệ</Nav.Link>
+      
+     </Nav>
+      <Form className="d-flex">
+       {/* <FormControl
+         type="search"
+         placeholder="Search"
+         className="me-2"
+         aria-label="Search"
+       />  */}
+       <Button variant="outline-success" className='onli'>Login</Button>
+      
+     </Form>
+   </Navbar.Collapse>
+ </Container>
+</Navbar>
+
+   </div>
+   
+  )
+}
+
+export default Nabar
