2f1057b7ce3ad9f61c0680629fd8ea40da092003
vietnguyen99 <nguyenducviet888999@gmail.com>
2023-03-02 11:19:50 +0700

    lam giao dien trang home

--- clinic-frontend/src/layout/Footer.jsx
+++ clinic-frontend/src/layout/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './Footer.css'
+import { FaFacebook } from 'react-icons/fa';
+import {BsInstagram} from 'react-icons/bs';
+import {SiZalo} from 'react-icons/si'
 
 export default function Footer() {
   return (
+   <section className='section footer bg-dark text-white'>
     <div class="container text-center">
   <div class="row">
-    <div class="col">
+    <div class="col-md-4">
     <h4>THÔNG TIN LIÊN HỆ</h4>
     <div className="footer-left">
         <p>NHA KHOA TRỒNG RĂNG</p>
@@ -12,13 +17,16 @@ export default function Footer() {
         <p> Điện thoại:</p>
     </div>
     </div>
-    <div class="col">
+    <div class="col-md-4">
     <h4>THỜI GIAN LÀM VIỆC</h4>
     <p> Thứ 2-7: 8:00 – 20:00</p>
     <p>Chủ nhật: 8:00 – 17:00</p>
     <p>Không nghỉ trưa</p>
+    <span><FaFacebook /></span>
+    <span><BsInstagram/> </span>
+    <samp><SiZalo/></samp>
     </div>
-    <div class="col">
+    <div class="col-md-4">
     <h4>CÁC CHÍNH SÁCH – HỖ TRỢ</h4>
     <p> Chính sách bảo mật</p>
     <p> Chính sách thanh toán</p>
@@ -29,5 +37,6 @@ export default function Footer() {
     </div>
   </div>
 </div>
+</section>
   )
 }
--- clinic-frontend/src/page/home/Home.jsx
+++ clinic-frontend/src/page/home/Home.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
+import './Home.css';
 import Slider from '../../layout/Slider';
 import Footer from '../../layout/Footer'
+import imgs1 from "../../img/imgs1.png"
+import imgs2 from "../../img/imgs2.png"
+import imgs3 from "../../img/imgs3.png"
+import imgs4 from "../../img/imgs4.png"
 
 export default function Home() {
   return (
    <div>
     <Slider />
+     
+        
+     <section>
+        <div class="container text-center">
+  <div class="row">
+    <div class="col-md-3">
+    <img src={imgs1} alt='...' className='imgs_left_img' />
+    <h5 className='about-us'>BÁC SĨ KINH NGHIỆM</h5>
+    <p>Đội ngũ y bác sĩ là chuyên gia với nền tảng chuyên môn vững chắc và giàu kinh nghiện và y đức</p>
+    </div>
+    <div class="col-md-3">
+    <img src={imgs2} alt='...' className='imgs_left_img' />
+    <h5 className='about-us'>Công Nghệ Hiện Đại </h5>
+    <p>Hệ thống trang thiết bị y tế hiện đại</p> bật nhất được nhập khẩu từ các nước Anh, Đức, Hà Lam, pháp 
+    </div>
+    <div class="col-md-3">
+    <img src={imgs3} alt='...' className='imgs_left_img' />
+    <h5 className='about-us'>Chi phí hợp lý</h5>
+    <p>Chi phí khám và điệu trị, phẩu thuật minh bạch rõ ràng, cùng nhiều ưu đãi hổ trợ hấp dẫn</p>
+    </div>
+    <div class="col-md-3">
+    <img src={imgs4} alt='...' className='imgs_left_img' />
+    <h5 className='about-us'>Địa chỉ</h5>
+    <p>Quy Nhơn Bình Định</p>
+    </div>
+  </div>
+</div>
+        </section>
+      
+
+    
+
     <Footer />
    </div>
     
