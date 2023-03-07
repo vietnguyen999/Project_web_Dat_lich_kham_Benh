465786b00109b69f44d9dc3255534081770bc848
vietnguyen99 <nguyenducviet888999@gmail.com>
2023-03-07 09:59:10 +0700

    làm giao dien trang trong răng implant

--- clinic-frontend/src/page/news/News.jsx
+++ clinic-frontend/src/page/news/News.jsx
@@ -1,7 +1,53 @@
 import React from 'react'
-
+import './News.css';
+import {Row,Col} from 'react-bootstrap';
+import imgs24 from "../../img/imgs24.png"
+import imgs25 from "../../img/imgs25.png"
+import Footer from '../../layout/Footer'
 export default function News() {
   return (
-    <div>News</div>
+    <div>
+
+<div className="header-rangsu">
+            <p className='header-rangsu-p'>Trang chủ <span className='header-rangsu-dau' >-</span>Răng Sứ</p>
+        </div>
+        <Row className='section-rangsu'>
+        <Col className='section-rangsu-left' xs={12} md={8}>
+         <h2 className='section-rangsu-left-h2'>TRỒNG RĂNG IMPLANT</h2>
+         <p className='section-rangsu-left-p'>Tìm được 2 bài viết</p>
+            <Row className="section-rangsu-left-tin">
+         <Col className=' section-rangsu-left-tin-left' xs={12} md={6}>
+            <img src={imgs24} className='section-rangsu-left-tin-left-img'/>
+            <h3 className='section-rangsu-left-tin-left-h3'>TRỒNG RĂNG TOÀN HÀM</h3>
+            <p className='section-rangsu-left-tin-left-p'>
+            Trồng răng Implant toàn hàm với kỹ thuật All on 4 hay All on 6 hiện nay được 
+            đánh giá mang lại hiệu quả cao khi phục hồi tình trạng mất răng toàn...</p>
+            <button className='section-rangsu-left-tin-left-btn'>XEM CHI TIẾT...</button>
+         </Col>
+         <Col className=' section-rangsu-left-tin-left' xs={12} md={6}>
+         <img src={imgs25} className='section-rangsu-left-tin-left-img'/>
+            <h3 className='section-rangsu-left-tin-left-h3'>CẤY GHÉP IMPLANT</h3>
+            <p className='section-rangsu-left-tin-left-p'>
+            Cấy ghép Implant có khả năng phục hồi răng mang lại hiệu quả cải thiện tốt như
+             mong đợi của khách hàng trong các trường hợp Mất một răng, Mất nhiều răng, Mất toàn hàm....</p>
+            <button className='section-rangsu-left-tin-left-btn'>XEM CHI TIẾT...</button>
+         </Col>
+         </Row>
+        </Col>
+        <Col className='section-rangsu-right' xs={6} md={4}>
+            <h4 className='section-rangsu-right-h4'>DỊCH VỤ</h4>
+         <div className="section-rangsu-right-dichvu">
+            <h5 className='section-rangsu-right-dichvu-h5'>NHA KHOA TỔNG QUÁT</h5>
+            <h5 className='section-rangsu-right-dichvu-h5'>TRỒNG RĂNG IMPLANT</h5>
+            <h5 className='section-rangsu-right-dichvu-h5'>CHỈNH NHA - NIỀNG RĂNG</h5>
+            <h5 className='section-rangsu-right-dichvu-h5'>RĂNG SỨ THẪM MỸ</h5>
+
+         </div>
+        </Col>
+      </Row>
+
+       <Footer/>
+
+    </div>
   )
 }
