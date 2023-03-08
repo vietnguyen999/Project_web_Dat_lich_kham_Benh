21f5f50bed09e94f9075292e6f6d5b67d45328ab
vietnguyen99 <nguyenducviet888999@gmail.com>
2023-03-08 15:24:15 +0700

    lam @media trang home

--- clinic-frontend/src/layout/Slider.js
+++ clinic-frontend/src/layout/Slider.js
@@ -7,6 +7,7 @@ import Carousel from 'react-bootstrap/Carousel';
 
 function Slider() {
   return (
+    <div className='carouself'>
     <Carousel>
       <Carousel.Item>
         <img
@@ -14,10 +15,6 @@ function Slider() {
           src={img2}
           alt="First slide"
         />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -25,11 +22,6 @@ function Slider() {
           src={img1}
           alt="Second slide"
         />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
@@ -37,16 +29,9 @@ function Slider() {
           src={img3}
           alt="Third slide"
         />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
-    
+    </div>
   )
 }
 
--- clinic-frontend/src/page/home/Home.jsx
+++ clinic-frontend/src/page/home/Home.jsx
@@ -26,45 +26,39 @@ export default function Home() {
         
      <section>
         <div class="container text-center">
-  <div class="row">
-    <div class="col-md-3">
+  <div class="row sectiopn-information-clinic">
+    <div class="col-md-3 sectiopn-information">
     <img src={imgs1} alt='...' className='imgs_left_img' />
-
     <div className="text-section">
    <h4 className='text-section-h4'>BÁC SĨ KINH NGIỆN</h4>
    <p className='text-section-p'>Đội ngũ bác sĩ là chuyên gia với nền tảng chuyên môn vững chắc, giàu kinh nghiện và y đức</p>
    </div>
-
-
     </div>
-    <div class="col-md-3">
+
+    <div class="col-md-3 sectiopn-information">
     <img src={imgs2} alt='...' className='imgs_left_img' />
-    
     <div className="text-section">
    <h4 className='text-section-h4'>CÔNG NGHỆ HIỆN ĐẠI</h4>
    <p className='text-section-p'>Hệ thóng trang thiết bị hiện đại bậc nhất được nhập khẩu từ các nước Hà lan,Anh, Đức, Mỹ</p>
    </div>
-    
     </div>
-    <div class="col-md-3">
+
+    <div class="col-md-3 sectiopn-information">
     <img src={imgs3} alt='...' className='imgs_left_img' />
-   
     <div className="text-section">
    <h4 className='text-section-h4'>CHI PHÍ HỢP LÝ</h4>
    <p className='text-section-p'>Chi phí khám và điều trị, phẫu thuật minh bạch, rõ ràng cùng nhiêu ưu đại hổ trợ hấp dẫn</p>
    </div>
-
     </div>
-    <div class="col-md-3">
+
+    <div class="col-md-3 sectiopn-information">
     <img src={imgs4} alt='...' className='imgs_left_img' />
-   
     <div className="text-section">
    <h4 className='text-section-h4'>ĐẠI CHỈ</h4>
    <p className='text-section-p'>QUY NHƠN, BINH ĐỊNH</p>
    </div>
-
-
     </div>
+
   </div>
 </div>
         </section>
@@ -127,7 +121,7 @@ export default function Home() {
             <div className="col">
               <div className="section-datlich-right">
                  <img src={imgs9} alt='...' className='section-datlich-right-img'/>
-                 <h1 className='section-datlich-right-h1'>Nha khoa </h1>
+                 <h1 className='section-datlich-right-h1'>Nha khoa Viet Nguyen</h1>
               </div>
             </div>
           </div>
