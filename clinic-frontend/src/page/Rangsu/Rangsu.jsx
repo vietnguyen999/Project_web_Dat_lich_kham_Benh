50f37914e4b668efd43bbef66f4b25eff0ce29bd
vietnguyen99 <nguyenducviet888999@gmail.com>
2023-03-15 14:21:16 +0700

    day code lenh nhanh viet các trang trong nha khoa tong quat

--- clinic-frontend/src/page/Rangsu/Rangsu.jsx
+++ clinic-frontend/src/page/Rangsu/Rangsu.jsx
@@ -34,14 +34,15 @@ export default function Rangsu() {
          </Col>
          </Row>
         </Col>
-        <Col className='section-rangsu-right' xs={6} md={4}>
+        <Col className='section-rangsu-right'  xs={6} md={4}>
             <h4 className='section-rangsu-right-h4'>DỊCH VỤ</h4>
-         <div className="section-rangsu-right-dichvu">
-            <h5 className='section-rangsu-right-dichvu-h5'>NHA KHOA TỔNG QUÁT</h5>
+         <div className="section-rangsu-right-dichvu" >
+          
+           <h5 className='section-rangsu-right-dichvu-h5'>NHA KHOA TỔNG QUÁT</h5>
             <h5 className='section-rangsu-right-dichvu-h5'>TRỒNG RĂNG IMPLANT</h5>
             <h5 className='section-rangsu-right-dichvu-h5'>CHỈNH NHA - NIỀNG RĂNG</h5>
             <h5 className='section-rangsu-right-dichvu-h5'>RĂNG SỨ THẪM MỸ</h5>
-
+           
          </div>
         </Col>
       </Row>
